83e8069fdbda6084590af24f9405d8f88c8eaeee
Sherzod <sherzod@77projects.com>
2021-05-27 18:03:07 +0500

    home_page

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,28 @@
 $(function () {
-  $('.categories').owlCarousel({
-    items: 2
+  function slider(selector) {
+    $(selector).owlCarousel({
+      items: 2,
+      dots: false,
+      smartSpeed: 1000,
+      mouseDrag: false,
+      touchDrag: false
+    })
+  }
+
+  slider('.categories')
+  slider('.news__slider')
+
+  const heroSliderBtns = [
+    '<span class="icon-sl"></span>',
+    '<span class="icon-sl inverse"></span>'
+  ]
+
+  $('.hero__slider').owlCarousel({
+    items: 1,
+    nav: true,
+    navText: heroSliderBtns,
+    dots: false,
+    mouseDrag: false,
+    touchDrag: false
   })
 })
